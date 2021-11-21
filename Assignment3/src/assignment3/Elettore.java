ff7e10f2640e800c8b6112e874b6749596b426a3
Lorenzo <lorenzo.romeo.mail@gmail.com>
2021-11-21 11:29:31 +0100

    Added showInfo and fixed JML

--- Assignment3/src/assignment3/Elettore.java
+++ Assignment3/src/assignment3/Elettore.java
@@ -1,5 +1,7 @@
 package assignment3;
 
+import java.util.Arrays;
+
 class Date {
 	public int day;
 	public int month;
@@ -9,6 +11,10 @@ class Date {
 		this.day = day;
 		this.month = month;
 		this.year = year;
+	}
+	
+	public String toString(){
+		return day+"/"+month+"/"+year;
 	}
 	
 }
@@ -24,8 +30,8 @@ public class Elettore {
 	private /*@ spec_public @*/ Date dataDiNascita;
 	private /*@ spec_public @*/ String comuneDiNascita;
 
-	/*@ public invariant 	(nazioneDiNascita.equals("Italia") && 
-	  @ 					comuneDiNascita != null) || (!comuneDiNascita.equals("Italia"));
+	/*@ public invariant 	(nazioneDiNascita.equals("Italia") &&  !comuneDiNascita.equals(" ")) 
+	  @						|| (!nazioneDiNascita.equals("Italia"));
 	  @*/
 	private /*@ spec_public @*/ String nazioneDiNascita;
 
@@ -42,13 +48,15 @@ public class Elettore {
 	// Se l'utente ha gia' espresso il voto ha valore true
 	private /*@ spec_public @*/ boolean voto;
 
+	
+	
 	public Elettore(String nome, String cognome, int giorno, int mese, int anno, String sesso,
 			String comuneDiNascita, String nazioneDiNascita, char[] codiceFiscale) {
 		this.nome = nome;
 		this.cognome = cognome;
 		this.dataDiNascita = new Date(giorno, mese, anno);
 		this.sesso = sesso;
-		this.comuneDiNascita = comuneDiNascita;
+		this.comuneDiNascita = (comuneDiNascita == null)?" ":comuneDiNascita;
 		this.nazioneDiNascita = nazioneDiNascita;
 		this.codiceFiscale = codiceFiscale;
 		this.oggi = new Date(17, 11, 2021);
@@ -65,12 +73,29 @@ public class Elettore {
 		this.voto = true;
 	}
 	
+	public void showInfo(){
+		System.out.println("---");
+		System.out.println("Today = "+oggi);
+		System.out.println("Nome, cognome = "+nome+" "+cognome);
+		System.out.println("Data di nascita = "+dataDiNascita);
+		System.out.println("Nazione di nascita= "+ nazioneDiNascita);
+		System.out.println("Comune di nascita = "+comuneDiNascita);
+		System.out.println("Codice fiscale = "+Arrays.toString(codiceFiscale));
+		System.out.println("Has voted = "+((voto)?"Yes":"No"));
+		System.out.println("---");
+	}
+	
 	
 	public static void main(String[] argv) {
 		char[] f1 = {'A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A'};
 			//Elettore elettore = new Elettore("Dianka Mevan", "Fernando", 4, 10, 2020, "J", "Milano", "Italia", f1);
 			Elettore elettore2 = new Elettore("Lorenzo", "Romeo", 5, 10, 1800, "M", "Milano", "Italia", f1);
+			
+			//TODO null da errore anche se l'invariante e' rispettata
+			Elettore elettore3 = new Elettore("A", "B", 5, 10, 1800, "M", null, "Svezia", f1);
+			elettore2.showInfo();
 			elettore2.esprimi_voto();
+			elettore2.showInfo();
 			//elettore2.esprimi_voto();
 	}
 
